0cbebb3e446f24c80d11049fab8eb74683155c23
Ethan Evans <eevans@mit.edu>
2020-06-08 17:59:27 -0400

    Delete IPO_param_picking_old_commandline.R

--- 3_xcms/old/IPO_param_picking_old_commandline.R
+++
@@ -1,9 +0,0 @@
-library(IPO)
-mz <- read.csv('mzData.txt', stringsAsFactor=FALSE)$fname
-peakpickingParameters <- getDefaultXcmsSetStartingParams('centWave')
-time.xcmsSet <- system.time({resultPeakpicking <- optimizeXcmsSet(files = mz[5], params = peakpickingParameters, isotopeIdentification="CAMERA", nSlaves = 2, subdir = NULL, plot = FALSE)})
-
-optimizedXcmsSetObject <- resultPeakpicking$best_settings$xset
-retcorGroupParameters <- getDefaultRetGroupStartingParams()
-time.RetGroup <- system.time({resultRetcorGroup <- optimizeRetGroup(xset = optimizedXcmsSetObject, params = retcorGroupParameters, nSlaves = 1, subdir = NULL, plot = FALSE)})
-writeRScript(resultPeakpicking$best_settings$parameters, resultRetcorGroup$best_settings)
